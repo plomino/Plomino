d5eba50244281cc5ce98391884caf7bd5dd25e5e
ebrehault <ebrehault@39f4f2d6-6c1e-0410-acfa-ed0e10f9290e>
2010-02-19 08:33:23 +0000

    Google Visualization table widget for doclink fields
    git-svn-id: https://plomino.svn.sourceforge.net/svnroot/plomino/trunk@625 39f4f2d6-6c1e-0410-acfa-ed0e10f9290e

--- Plomino/Products/CMFPlomino/fields/doclink.py
+++ Plomino/Products/CMFPlomino/fields/doclink.py
@@ -27,7 +27,8 @@ class IDoclinkField(IBaseField):
     """
     widget = Choice(vocabulary=SimpleVocabulary.fromItems([("Selection list", "SELECT"),
                                                            ("Multi-selection list", "MULTISELECT"),
-                                                           ("Embedded view", "VIEW")
+                                                           ("Embedded view", "VIEW"),
+                                                           ("Google Visualization table", "GTABLE")
                                                            ]),
                     title=u'Widget',
                     description=u'Field rendering',
@@ -48,6 +49,10 @@ class IDoclinkField(IBaseField):
     separator = TextLine(title=u'Separator',
                       description=u'Only apply if multi-valued',
                       required=False)
+    googletableparam = TextLine(title=u'Google table parameters',
+                    description=u'See Google visualization documentation',
+                    default=u"showRowNumber: false",
+                    required=False)
     
 class DoclinkField(BaseField):
     """
@@ -68,7 +73,7 @@ class DoclinkField(BaseField):
             if v is None:
                 return []
             else:
-                return [getattr(d, 'PlominoViewColumn_'+self.sourceview+'_'+self.labelcolumn)+"|"+d.getPath() for d in v.getAllDocuments()]
+                return [getattr(d, 'PlominoViewColumn_'+self.sourceview+'_'+self.labelcolumn, '')+"|"+d.getPath() for d in v.getAllDocuments()]
         else:
             #if no doc provided (if OpenForm action), we use the PlominoForm
             if doc is None:
@@ -89,6 +94,83 @@ class DoclinkField(BaseField):
             else:
                 proper.append(v+'|'+v)
         return proper
+
+    def processInput(self, submittedValue):
+        """
+        """
+        if "|" in submittedValue:
+            return submittedValue.split("|")
+        else:
+            return submittedValue
+        
+    def jscode(self, selectionlist, selected):
+        """ return Google visualization js code
+        """
+        field_id = self.context.id
+        if self.sourceview is not None:
+            sourceview = self.context.getParentDatabase().getView(self.sourceview)
+            alldocs = sourceview.getAllDocuments()
+            columns = sourceview.getColumns()
+            column_ids = [col.id for col in columns]
+            column_labels = [col.Title() for col in columns]
+            paths = [doc.getPath() for doc in alldocs]
+            datatable = [[getattr(doc, "PlominoViewColumn_%s_%s" % (self.sourceview, col)) for col in column_ids] for doc in alldocs]
+        else:
+            column_labels = [""]
+            paths = [v.split('|')[1] for v in selectionlist]
+            datatable = [[v.split('|')[0]] for v in selectionlist]
+            
+        loadjs = self.context.REQUEST.get('googlejsapi', False)
+        if not loadjs:
+            self.context.REQUEST.set('googlejsapi', True)
+            
+        js = """
+google.load('visualization', '1', {packages:['table']});
+google.setOnLoadCallback(drawTable);
+function drawTable() {\n"""
+        
+        js = js + "var %s_mapping = new Array(%s);\n" % (field_id,  ", ".join(["'"+p+"'" for p in paths]) )
+        if selected is None or len(selected)==0:
+            js_selected = ""
+        else:
+            js_selected = ", ".join(['{row:%d, column: null}' % paths.index(s) for s in selected if s in paths])
+            
+        js = js + "var %s_selected = new Array(%s);\n" % (field_id, js_selected )
+        
+        js = js + "var %s_data = new google.visualization.DataTable();\n" % (field_id)
+        
+        for col in column_labels:
+            #TODO: accept other types than string
+            js = js + "  %s_data.addColumn('string', '%s');\n" % (field_id, col)
+
+        js_table = ", ".join(["[" + ", ".join(["'"+cell+"'" for cell in row]) + "]" for row in datatable])
+        js = js + "  %s_data.addRows([%s]);\n" % (field_id, js_table)
+#        i = 0
+#        for row in datatable:
+#            j = 0
+#            for cell in row:
+#                js = js + field_id +"_data.setCell(" + str(i) + ", " + str(j) + ", '" + cell + "');\n"
+#                j = j + 1
+#            i = i + 1
+        js = js + """
+ var %s_table = new google.visualization.Table(document.getElementById('%s_div'));
+ %s_table.draw(%s_data, {%s});\n""" % (field_id, field_id, field_id, field_id, self.googletableparam)
+
+        js = js + "%s_table.setSelection(%s_selected);\n" % (field_id, field_id)
+        js = js + """
+                google.visualization.events.addListener(%s_table, 'select',
+                  function(event) {
+                    selection = %s_table.getSelection();
+                    v = ""
+                    for(var i = 0; i < selection.length; i++) {
+                        v = v + %s_mapping[selection[i].row] + "|";
+                    }
+                    field = document.getElementById('%s');
+                    field.value = v;
+                  });
+        """ % (field_id, field_id, field_id, field_id)
+        js = js + "}"
+        return js
     
 for f in getFields(IDoclinkField).values():
     setattr(DoclinkField, f.getName(), DictionaryProperty(f, 'parameters'))
