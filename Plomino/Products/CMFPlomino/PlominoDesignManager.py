84727c4face305a554f70fb7bd40ac33d5cc8ec1
neaj <neaj@39f4f2d6-6c1e-0410-acfa-ed0e10f9290e>
2010-05-26 09:39:34 +0000

    Pass a dummy request, to enable calling from a ./bin/instance debug script or a debug session.
    git-svn-id: https://plomino.svn.sourceforge.net/svnroot/plomino/trunk@815 39f4f2d6-6c1e-0410-acfa-ed0e10f9290e

--- Plomino/Products/CMFPlomino/PlominoDesignManager.py
+++ Plomino/Products/CMFPlomino/PlominoDesignManager.py
@@ -738,7 +738,7 @@ class PlominoDesignManager(Persistent):
                 child = child.nextSibling
                 
             if len(at_values) > 0:
-                obj.processForm(values=at_values)
+                obj.processForm(REQUEST='dummy', values=at_values)
             if len(settings_values) > 0:
                 adapt = obj.getSettings()
                 for key in settings_values.keys():
