0f96585cdd55233a260c7717ee18fa1538c4e575
Eric BREHAULT <ebrehault@gmail.com>
2010-11-02 20:16:33 +0100

    clean up old code (ZpCron dependencies)

--- Products/CMFPlomino/PlominoAgent.py
+++ Products/CMFPlomino/PlominoAgent.py
@@ -21,12 +21,6 @@ from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
 
 from Products.CMFPlomino.config import *
 
-##code-section module-header #fill in your manual code here
-from Products.Archetypes.public import *
-from AccessControl.SecurityManagement import getSecurityManager, setSecurityManager, newSecurityManager
-import transaction
-##/code-section module-header
-
 schema = Schema((
 
     StringField(
@@ -49,48 +43,11 @@ schema = Schema((
             i18n_domain='CMFPlomino',
         ),
     ),
-    BooleanField(
-        name='Scheduled',
-        widget=BooleanField._properties['widget'](
-            label="Scheduled",
-            description="Scheduled agent",
-            label_msgid='CMFPlomino_label_Scheduled',
-            description_msgid='CMFPlomino_help_Scheduled',
-            i18n_domain='CMFPlomino',
-        ),
-    ),
-    StringField(
-        name='AgentUser',
-        widget=StringField._properties['widget'](
-            label="Agent user",
-            description="Effective user used to run the agent",
-            label_msgid='CMFPlomino_label_AgentUser',
-            description_msgid='CMFPlomino_help_AgentUser',
-            i18n_domain='CMFPlomino',
-        ),
-    ),
-    StringField(
-        name='Cron',
-        default='* 1 * * *',
-        widget=StringField._properties['widget'](
-            label="Cron configuration",
-            description="Format : crontab UNIX",
-            label_msgid='CMFPlomino_label_Cron',
-            description_msgid='CMFPlomino_help_Cron',
-            i18n_domain='CMFPlomino',
-        ),
-    ),
 ),
 )
 
-##code-section after-local-schema #fill in your manual code here
-##/code-section after-local-schema
-
 PlominoAgent_schema = BaseSchema.copy() + \
     schema.copy()
-
-##code-section after-schema #fill in your manual code here
-##/code-section after-schema
 
 class PlominoAgent(BaseContent, BrowserDefaultMixin):
     """
@@ -103,34 +60,11 @@ class PlominoAgent(BaseContent, BrowserDefaultMixin):
 
     schema = PlominoAgent_schema
 
-    ##code-section class-header #fill in your manual code here
-    ##/code-section class-header
-
-    # Methods
-
-    security.declarePublic('at_post_create_script')
-    def at_post_create_script(self):
-        """
-        """
-        db = self.getParentDatabase()
-        db.managePlominoCronTab()
-
     security.declarePublic('at_post_edit_script')
     def at_post_edit_script(self):
         """
         """
         self.cleanFormulaScripts("agent_"+self.id)
-        #cron tab
-        db = self.getParentDatabase()
-        db.managePlominoCronTab()
-
-    security.declarePublic('manage_beforeDelete')
-    def manage_beforeDelete(self,item,container):
-        """
-        """
-        #cron tab
-        db = self.getParentDatabase()
-        db.managePlominoCronTab(True)
 
     security.declarePublic('getParentDatabase')
     def getParentDatabase(self):
@@ -141,13 +75,10 @@ class PlominoAgent(BaseContent, BrowserDefaultMixin):
     def __call__(self, *args):
         """
         """
-        txn = transaction.get()
         plominoContext = self
         try:
             result = self.runFormulaScript("agent_"+self.id, plominoContext, self.Content, True, *args)
-            txn.commit()
         except Exception, e:
-            txn.abort()
             result = "Error: %s \nCode->\n%s" % (e, self.Content())
             
         return result
@@ -156,22 +87,15 @@ class PlominoAgent(BaseContent, BrowserDefaultMixin):
     def runAgent(self,REQUEST=None):
         """execute the python code
         """
-
-        #acl = self.acl_users
-        #newSecurityManager(None, acl.getUserById(str(self.AgentUser), None))
-        txn = transaction.get()
         plominoContext = self
         plominoReturnURL = self.getParentDatabase().absolute_url()
         try:
-            #RunFormula(plominoContext, "agent_"+self.id, self.Content())
             r=self.runFormulaScript("agent_"+self.id, plominoContext, self.Content)
-            txn.commit()
             if (REQUEST != None) and (REQUEST.get('REDIRECT', None)== "True"):
                 if r is not None:
                     plominoReturnURL=r
                 REQUEST.RESPONSE.redirect(plominoReturnURL)
         except Exception, e:
-            txn.abort()
             return "Error: %s \nCode->\n%s" % (e, self.Content())
 
 
