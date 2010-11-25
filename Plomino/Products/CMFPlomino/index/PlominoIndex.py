707108f39e9894c02815c9f8fd00ae6e127a97cd
neaj <neaj@39f4f2d6-6c1e-0410-acfa-ed0e10f9290e>
2010-11-25 11:45:10 +0000

    Log errors during indexing.
    git-svn-id: https://plomino.svn.sourceforge.net/svnroot/plomino/trunk@1098 39f4f2d6-6c1e-0410-acfa-ed0e10f9290e

--- Plomino/Products/CMFPlomino/index/PlominoIndex.py
+++ Plomino/Products/CMFPlomino/index/PlominoIndex.py
@@ -131,7 +131,11 @@ class PlominoIndex(UniqueObject, ZCatalog, ActionProviderBase):
         """
         """
         #self.catalog_object(doc, "/".join(doc.getPhysicalPath()))
-        self.catalog_object(doc, "/".join(doc.getParentDatabase().getPhysicalPath())+"/"+doc.id)
+        try:
+          self.catalog_object(doc, "/".join(doc.getParentDatabase().getPhysicalPath())+"/"+doc.id)
+        except Exception, e:
+          self.plone_log('%s\non %s'%(`e`, doc.id))
+          raise
 
     security.declareProtected(READ_PERMISSION, 'unindexDocument')
     def unindexDocument(self,doc):
