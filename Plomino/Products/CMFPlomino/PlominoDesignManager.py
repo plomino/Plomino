89342a106fd853c77ef5d6a8ab179aa36c80477c
neaj <neaj@39f4f2d6-6c1e-0410-acfa-ed0e10f9290e>
2010-12-04 09:57:38 +0000

    Fix another browserism.
    git-svn-id: https://plomino.svn.sourceforge.net/svnroot/plomino/trunk@1129 39f4f2d6-6c1e-0410-acfa-ed0e10f9290e

--- Plomino/Products/CMFPlomino/PlominoDesignManager.py
+++ Plomino/Products/CMFPlomino/PlominoDesignManager.py
@@ -521,18 +521,19 @@ class PlominoDesignManager(Persistent):
                     plone_tools.addPortalMessage(msg, msgType, REQUEST)
 
     security.declarePublic('reportError')
-    def reportError(self, message, REQUEST=None, formula=None, path=None):
+    def reportError(self, message, request=None, formula=None, path=None):
         """
         """
-        if self.REQUEST:
-            REQUEST = self.REQUEST
-        if REQUEST:
+        if not request:
+            if hasattr(self, 'REQUEST'):
+                request = self.REQUEST
+        if request:
             if formula and hasattr(formula, 'absolute_url_path'):
                 path = formula.absolute_url_path()
             if path:
                 message = message + " - Plomino formula %s" % path
             plone_tools = getToolByName(self, 'plone_utils')
-            plone_tools.addPortalMessage(message, 'error', REQUEST)
+            plone_tools.addPortalMessage(message, 'error', request)
             
     security.declarePublic('getRenderingTemplate')
     def getRenderingTemplate(self, templatename, request=None):
