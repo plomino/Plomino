22294db636b84ad7266cdd6605d6bc01d10fcc22
ebrehault <ebrehault@39f4f2d6-6c1e-0410-acfa-ed0e10f9290e>
2011-02-11 14:25:09 +0000

    Plone 3 compliancy for validator
    git-svn-id: https://plomino.svn.sourceforge.net/svnroot/plomino/trunk@1181 39f4f2d6-6c1e-0410-acfa-ed0e10f9290e

--- Plomino/Products/CMFPlomino/validator.py
+++ Plomino/Products/CMFPlomino/validator.py
@@ -1,9 +1,22 @@
 from Products.validation import validation
-from Products.validation.interfaces.IValidator import IValidator
+
+try:
+    # Plone 4 and higher
+    import plone.app.upgrade
+    from Products.validation.interfaces.IValidator import IValidator
+    USE_BBB_VALIDATORS = False
+except ImportError:
+    # BBB Plone 3
+    from Products.validation.interfaces import ivalidator
+    USE_BBB_VALIDATORS = True
+    
 from zope.interface import implements
 
 class PlominoIdValidator:
-    implements(IValidator)
+    if USE_BBB_VALIDATORS:
+        __implements__ = (ivalidator,)
+    else:
+        implements(IValidator)
     
     def __init__(self, name, title='', description=''):
         self.name = name
