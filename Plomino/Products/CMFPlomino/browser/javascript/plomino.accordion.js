15c853e632e374518ee145cfa3b9cf026986ddae
ebrehault <ebrehault@39f4f2d6-6c1e-0410-acfa-ed0e10f9290e>
2011-04-14 13:30:42 +0000

    load accordion content when clicking it if url is provided
    git-svn-id: https://plomino.svn.sourceforge.net/svnroot/plomino/trunk@1303 39f4f2d6-6c1e-0410-acfa-ed0e10f9290e

--- Plomino/Products/CMFPlomino/browser/javascript/plomino.accordion.js
+++ Plomino/Products/CMFPlomino/browser/javascript/plomino.accordion.js
@@ -12,4 +12,13 @@ jq(document).ready(function() {
 	jq("h3.plomino-accordion-header").parent().accordion({
 		header: "h3.plomino-accordion-header", collapsible: true, autoHeight: false
 	});
+	
+	jq(".plomino-accordion-header").click(function(e) {
+        url = jq(this).find("a").attr("href");
+        if(url!="#") {
+           var contentDiv = jq(this).next("div");
+           contentDiv.load(jq(this).find("a").attr("href")+" #content");
+           jq(this).find("a").attr("href", "#");
+        }
+    });
 });
