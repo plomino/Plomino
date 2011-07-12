390458c2f829fe1dd13a449f1da6e0c9c1674ec3
neaj <neaj@39f4f2d6-6c1e-0410-acfa-ed0e10f9290e>
2011-07-12 04:49:53 +0000

    Fix whitespace.
    git-svn-id: https://plomino.svn.sourceforge.net/svnroot/plomino/trunk@1402 39f4f2d6-6c1e-0410-acfa-ed0e10f9290e

--- Plomino/Products/CMFPlomino/index/PlominoCatalog.py
+++ Plomino/Products/CMFPlomino/index/PlominoCatalog.py
@@ -15,37 +15,37 @@ from Missing import MV
 from PlominoUtils import asUnicode
 
 try:
-	from DocumentTemplate.cDocumentTemplate import safe_callable
+    from DocumentTemplate.cDocumentTemplate import safe_callable
 except ImportError:
-	# Fallback to python implementation to avoid dependancy on DocumentTemplate
-	def safe_callable(ob):
-		# Works with ExtensionClasses and Acquisition.
-		if hasattr(ob, '__class__'):
-			return hasattr(ob, '__call__') or isinstance(ob, types.ClassType)
-		else:
-			return callable(ob)
+    # Fallback to python implementation to avoid dependancy on DocumentTemplate
+    def safe_callable(ob):
+        # Works with ExtensionClasses and Acquisition.
+        if hasattr(ob, '__class__'):
+            return hasattr(ob, '__call__') or isinstance(ob, types.ClassType)
+        else:
+            return callable(ob)
 
 class PlominoCatalog(Catalog):
-	"""Plomino catalog (just overloads recordify method)
-	"""
+    """Plomino catalog (just overloads recordify method)
+    """
 
-	def recordify(self, object):
-		""" turns an object into a record tuple """
-		record = []
-		# the unique id is always the first element
-		for x in self.names:
-			if x.startswith("PlominoViewColumn_"):
-				param = x.split('_')
-				viewname = param[1]
-				columnname = param[2]
-				if not object.isSelectedInView(viewname):
-					v = None
-				else:
-					v = asUnicode(object.computeColumnValue(viewname, columnname))
-				record.append(v)
-			else:
-				attr=getattr(object, x, MV)
-				if attr is not MV and safe_callable(attr):
+    def recordify(self, object):
+        """ turns an object into a record tuple """
+        record = []
+        # the unique id is always the first element
+        for x in self.names:
+            if x.startswith("PlominoViewColumn_"):
+                param = x.split('_')
+                viewname = param[1]
+                columnname = param[2]
+                if not object.isSelectedInView(viewname):
+                    v = None
+                else:
+                    v = asUnicode(object.computeColumnValue(viewname, columnname))
+                record.append(v)
+            else:
+                attr=getattr(object, x, MV)
+                if attr is not MV and safe_callable(attr):
                     attr=attr()
-				record.append(asUnicode(attr))
-		return tuple(record)
+                record.append(asUnicode(attr))
+        return tuple(record)
