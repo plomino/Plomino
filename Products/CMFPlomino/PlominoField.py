4c9d343b48a74ddd8b083f56828a1611d734919a
Eric BREHAULT <ebrehault@gmail.com>
2011-10-11 11:05:34 +0200

    avoid error if bad file content upload in File field

--- Products/CMFPlomino/PlominoField.py
+++ Products/CMFPlomino/PlominoField.py
@@ -199,6 +199,8 @@ class PlominoField(BaseContent, BrowserDefaultMixin):
                 if new_file is not None:
                     current_files[new_file]=contenttype
                 v=current_files
+            else:
+                v = None
         else:
             v = adapt.processInput(submittedValue)
         return v
