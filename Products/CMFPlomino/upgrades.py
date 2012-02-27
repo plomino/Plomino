a197b965c047c42cd1eea63846b148df04bf5ac0
Eric BREHAULT <ebrehault@gmail.com>
2012-02-27 21:49:32 +0100

    upgrade step to register a forgotten javascript file

--- Products/CMFPlomino/upgrades.py
+++ Products/CMFPlomino/upgrades.py
@@ -1,5 +1,7 @@
 from Products.CMFCore.utils import getToolByName
 
+default_profile = 'profile-Products.CMFPlomino:default'
+
 def upgrade_22_to_23(context):
     #install collective.js.jqueryui as new dependency
     setup = getToolByName(context, 'portal_setup')
@@ -15,4 +17,9 @@ def upgrade_22_to_23(context):
     css_id = 'jquery-ui-1.7.3.custom.css'
     cssregistry = getToolByName(context, 'portal_css')
     cssregistry.unregisterResource(css_id)
-    
+
+def upgrade_to_28(context):
+    # register plomino.debug.js
+    # (it should have been done earlier, as plomino.debug.js was added in
+    # version 1.8)
+    context.runImportStepFromProfile(default_profile, 'jsregistry')
