de73248234d72a1af46fe92e0e6736a6e0b69a68
Silvio Tomatis <silviot@gmail.com>
2012-08-11 17:23:19 +0200

    Add test database for hideWhen test

--- Products/CMFPlomino/tests/test_functional.py
+++ Products/CMFPlomino/tests/test_functional.py
@@ -1,10 +1,21 @@
+from os.path import dirname, join
 import unittest2 as unittest
 
 from Products.CMFPlomino.testing import PLOMINO_FUNCTIONAL_TESTING
+
+DIRPATH = join(dirname(__file__), 'filestoimport')
 
 class IntegrationTest(unittest.TestCase):
 
     layer = PLOMINO_FUNCTIONAL_TESTING
 
-    def test_some_functionality(self):
+    def test_hide_when(self):
         "Dummy test"
+        # Import our sample database
+        mydb = self.layer['portal'].mydb
+        import_file('hideWhenTest.xml', mydb)
+
+
+def import_file(filename, plominodb):
+    filepath = join(DIRPATH, filename)
+    plominodb.importDesignFromXML(open(filepath).read())
