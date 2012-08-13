9ace7c0589c00ead5135e8615a390699a7bc3350
Silvio Tomatis <silviot@gmail.com>
2012-08-13 18:27:03 +0200

    Wait for jQuery ajax calls. Properly close error popup.

--- Products/CMFPlomino/tests/test_functional.py
+++ Products/CMFPlomino/tests/test_functional.py
@@ -1,5 +1,9 @@
 from os.path import dirname, join
+from datetime import datetime
+import time
+
 import unittest2 as unittest
+
 from plone.app.testing import selenium_layers
 from plone.app.testing import TEST_USER_NAME, TEST_USER_PASSWORD
 
@@ -28,18 +32,38 @@ class IntegrationTest(unittest.TestCase):
         selenium_layers.type(sel, 'a', '123')
         selenium_layers.click(sel, "//option[@value='A']")
         # Submitting now should warn us that 'c' has no value
-        selenium_layers.submit(sel, 'plomino_form')
+        sel.find_element_by_xpath("//input[@name='plomino_save']").click()
         popup = sel.find_element_by_id('plominoValidationPopup')
         sel.find_element_by_xpath("//strong[contains(text(), 'c is mandatory')]")
+        self.close_error_popup()
         # But when we fill it in, it should work
         selenium_layers.type(sel, 'c', 'A value for c!')
-        selenium_layers.submit(sel, 'plomino_form')
+        sel.find_element_by_xpath("//input[@name='plomino_save']").click()
+        # At this point the usual selenium wait-for-page-loaded doesn't work
+        # because we intercept the submit event and do ajax validation
+        self.wait_ajax_calls()
         # Empty hidden required fields should not trigger an error
         # XXX Here we should check nonzero length on mydb.plomino_documents.objectIds()
         # but for some reasons (isolated transacitons?) it doesn't show up
         self.assertTrue('plomino_documents' in sel.current_url)
 
+    def close_error_popup(self):
+        sel = self.layer['selenium']
+        sel.find_element_by_xpath("//div[@aria-labelledby='ui-dialog-title-plominoValidationPopup']"
+                                  "//a[contains(@class, 'ui-dialog-titlebar-close')]").click()
 
+    def wait_ajax_calls(self, timeout=5000, step=100):
+        "Wait until all jQuery initiated ajax calls return"
+        start = datetime.now()
+        elapsed_milliseconds = 0
+        while elapsed_milliseconds < timeout:
+            res = self.layer['selenium'].execute_script("return jQuery.active")
+            if not res:
+                return
+            time.sleep(step/1000.0)
+            now = datetime.now() - start
+            elapsed_milliseconds = now.microseconds/1000 + now.seconds*1000
+        self.fail("Timeout %i reached" % timeout)
 
 
 def import_file(filename, plominodb):
