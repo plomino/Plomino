0220b05afaa71b32c4aef95992309f9c79d1a176
Silvio Tomatis <silviot@gmail.com>
2013-06-28 18:30:32 +0200

    Basic upload progress bar

--- Products/CMFPlomino/browser/javascript/file_upload.js
+++ Products/CMFPlomino/browser/javascript/file_upload.js
@@ -6,19 +6,33 @@ jQuery(function() {
             'url': 'upload_to_session',
             add: function(e, data) {
                 console.log(data);
-                data.context = jQuery('<p/>').text('Uploading ' + data.files[0].name);
+                data.progress = jQuery('<div/>').addClass('progress');
+                progress_bar = jQuery('<div/>').addClass('bar');
+                data.progress.append(progress_bar);
+                data.context = jQuery('<p/>').text('Uploading ' +
+                    data.files[0].name + ' ' + data.files[0].size/1000 + ' kb');
                 data.context.insertAfter(this);
+                data.progress.insertAfter(this);
                 data.submit();
             },
             done: function (e, data) {
+                var container = jQuery(this).parent();
                 var hidden_name = "_uploaded_in_session_" + data.fileInput.attr('name');
                 var node = jQuery("<input type='hidden'/>").attr('name', hidden_name).attr('value', data.result.file_id);
-                data.context.text(data.files[0].name);
-                data.context.append(node);
-                if (! data.context.find("input[name=_has_session_uploads").length) {
-                    data.context.append(jQuery("<input type='hidden' name='_has_session_uploads', value='yes'>"))
+                container.append(node);
+                data.progress.detach();
+                data.context.detach();
+                container.append(jQuery("<div>").addClass('upload-status').text(data.files[0].name + " uploaded"))
+                if (! container.find("input[name=_has_session_uploads]").length) {
+                    container.append(jQuery("<input type='hidden' name='_has_session_uploads', value='yes'>"));
                 }
+            },
+            progress: function (e, data) {
+                var progress = parseInt(data.loaded / data.total * 100, 10);
+                var container = jQuery(this).parent();
+                container.find('.progress .bar').css('width', progress + '%');
             }
+
         });
     }
 });
