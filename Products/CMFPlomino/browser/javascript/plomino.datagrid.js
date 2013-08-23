3c7b9ab19e4ca47fdad1f2e8d845f97a6fe466cd
Michael Davis <m.r.davis@me.com>
2013-08-23 13:00:50 +0100

    Add a function as IE8 does not support object.keys

--- Products/CMFPlomino/browser/javascript/plomino.datagrid.js
+++ Products/CMFPlomino/browser/javascript/plomino.datagrid.js
@@ -65,6 +65,7 @@ function datagrid_show_form(field_id, formurl, onsubmit) {
 		var table = $("#" + field_id + "_datagrid");
 		var options = table.dataTable().fnSettings().oInit;
 		if(options.plominoDialogOptions) {
+            Object.keys = Object.keys || ie8_object_keys();
 			keys = Object.keys(options.plominoDialogOptions);
 			for(var k in keys) {
 				popup.dialog("option", keys[k], options.plominoDialogOptions[keys[k]]);
@@ -196,4 +197,44 @@ function datagrid_delete_row(table, field_id) {
 	else {
 		alert('You must select a row to delete.');
 	}
+}
+
+/*
+ * IE8 does not support Object.keys.
+ * Function from here:
+ * http://whattheheadsaid.com/2010/10/a-safer-object-keys-compatibility-implementation
+ */
+function ie8_object_keys () {
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !{toString:null}.propertyIsEnumerable("toString"),
+        DontEnums = [
+            'toString',
+            'toLocaleString',
+            'valueOf',
+            'hasOwnProperty',
+            'isPrototypeOf',
+            'propertyIsEnumerable',
+            'constructor'
+        ],
+        DontEnumsLength = DontEnums.length;
+
+    return function (o) {
+        if (typeof o != "object" && typeof o != "function" || o === null)
+            throw new TypeError("Object.keys called on a non-object");
+
+        var result = [];
+        for (var name in o) {
+            if (hasOwnProperty.call(o, name))
+                result.push(name);
+        }
+
+        if (hasDontEnumBug) {
+            for (var i = 0; i < DontEnumsLength; i++) {
+                if (hasOwnProperty.call(o, DontEnums[i]))
+                    result.push(DontEnums[i]);
+            }
+        }
+
+        return result;
+    };
 }
