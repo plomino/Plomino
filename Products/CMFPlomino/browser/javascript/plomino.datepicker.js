1449f2b776d96f85ad0e41c70297c02430ef9f22
Manuele Pesenti <manuele.pesenti@gmail.com>
2014-03-25 18:37:47 +0100

    DATETIME considered as single input field in case JQUERY widget choosen

--- Products/CMFPlomino/browser/javascript/plomino.datepicker.js
+++ Products/CMFPlomino/browser/javascript/plomino.datepicker.js
@@ -1,29 +1,16 @@
-function update(obj/*, …*/) {
-    for (var i=1; i<arguments.length; i++) {
-        for (var prop in arguments[i]) {
-            var val = arguments[i][prop];
-            if (typeof val == "object") // this also applies to arrays or null!
-                update(obj[prop], val);
-            else
-                obj[prop] = val;
-        };
-    };
-    return obj;
-};
-
 $(document).ready(function() {
-    //datepicker widget
+    // datepicker widget
     $(".date").datepicker().each(function(){
         var value = $(this).val();
         eval("var options = "  + $(this).data('datepickerOptions')+" ||{}");
         if ($(this).data('datepickerDateformat')&&!('dateFormat' in options)) {
-            // in case you use unusual date format not easily convertible between jQuery dateFormat and python
+            /* in case you use unusual date format not easily convertible
+               between jQuery dateFormat and python datetime format */
             options['dateFormat'] = $(this).data('datepickerDateformat');
         };
         if ( $(this).data('datepickerMindate') ) {
             options['minDate'] = $(this).data('datepickerMindate');
         };
-        console.log(options);
         $(this).datepicker( "option", options );
         $(this).datepicker( "setDate", value );
         $('#btn_' + this.id).click(function() {
