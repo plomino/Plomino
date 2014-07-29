7727bfab1509661207a5d00666077d184d2815fc
Roberto <roberto.starnini@gmail.com>
2014-07-29 18:15:54 +0200

    Fix null options bug

--- Products/CMFPlomino/browser/javascript/plomino.datepicker.js
+++ Products/CMFPlomino/browser/javascript/plomino.datepicker.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     // datepicker widget
     $(".date").datepicker().each(function(){
         var value = $(this).val();
-        eval("var options = "  + $(this).data('datepickerOptions')+" ||{}");
+        var options = $(this).data('datepickerOptions');
+        if(typeof(options)!='object') options = {};
         if ($(this).data('datepickerDateformat')&&!('dateFormat' in options)) {
             /* in case you use unusual date format not easily convertible
                between jQuery dateFormat and python datetime format */
@@ -13,8 +14,6 @@ $(document).ready(function() {
         };
         $(this).datepicker( "option", options );
         $(this).datepicker( "setDate", value );
-        $('#btn_' + this.id).click(function() {
-            $(this).datepicker('show');
-        });
+
     });
 });
