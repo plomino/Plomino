1d84ac513bbf11824d1b4111496375cbad39dea4
Gagaro <gagaro42@gmail.com>
2015-08-13 17:42:46 +0200

    feat: WIP add dialog to add field/action

--- src/Products/CMFPlomino/browser/static/js/tinymce.js
+++ src/Products/CMFPlomino/browser/static/js/tinymce.js
@@ -1,30 +1,133 @@
-tinymce.PluginManager.add('plomino', function(editor, url) {
-    var isNotFormEditor = !$('body').hasClass('portaltype-plominoform');
+(function() {
+    tinymce.PluginManager.add('plomino', function(editor, url) {
+        var isNotFormEditor = !$('body').hasClass('portaltype-plominoform');
 
-    if (isNotFormEditor) {
-        return ;
-    }
+        if (isNotFormEditor) {
+            return ;
+        }
 
-    // Add a button that opens a window
-    editor.addButton('plominofield', {
-        image: '++resource++Products.CMFPlomino/img/PlominoField.png',
-        onclick: function () {console.log('TODO')}
-    });
-    editor.addButton('plominoaction', {
-        image: '++resource++Products.CMFPlomino/img/PlominoAction.png',
-        onclick: function () {console.log('TODO')}
-    });
-    editor.addButton('plominosubform', {
-        image: '++resource++Products.CMFPlomino/img/PlominoForm.png',
-        onclick: function () {console.log('TODO')}
-    });
-    editor.addButton('plominohidewhen', {
-        image: '++resource++Products.CMFPlomino/img/PlominoHideWhen.png',
-        onclick: function () {console.log('TODO')}
-    });
-    editor.addButton('plominocache', {
-        image: '++resource++Products.CMFPlomino/img/PlominoCache.png',
-        onclick: function () {console.log('TODO')}
+        // Add a button that opens a window
+        editor.addButton('plominofield', {
+            image: '++resource++Products.CMFPlomino/img/PlominoField.png',
+            onclick: function () { editFormElement(editor, url, 'field'); }
+        });
+        editor.addButton('plominoaction', {
+            image: '++resource++Products.CMFPlomino/img/PlominoAction.png',
+            onclick: function () { editFormElement(editor, url, 'action'); }
+        });
+        editor.addButton('plominosubform', {
+            image: '++resource++Products.CMFPlomino/img/PlominoForm.png',
+            onclick: function () { editFormElement(editor, url, 'subform'); }
+        });
+        editor.addButton('plominohidewhen', {
+            image: '++resource++Products.CMFPlomino/img/PlominoHideWhen.png',
+            onclick: function () { editFormElement(editor, url, 'hidewhen'); }
+        });
+        editor.addButton('plominocache', {
+            image: '++resource++Products.CMFPlomino/img/PlominoCache.png',
+            onclick: function () { editFormElement(editor, url, 'cache'); }
+        });
+
     });
 
-});
+    var editFormElement = function(ed, url, elementType) {
+        // If the form is being created, don't create the same command
+        if (location.pathname.indexOf("++add++PlominoForm") != -1)
+        {
+            alert(ed.getLang('plomino_tinymce.edition_forbidden', 'Please save the form before using this button.'));
+            return;
+        }
+
+        if (elementType === "field") {
+            var elementClass = 'plominoFieldClass';
+            var elementEditionPage = '@@tinymceplominoform/field_form';
+            var elementIdName = 'fieldid';
+        }
+        else if (elementType === "action") {
+            var elementClass = 'plominoActionClass';
+            var elementEditionPage = '@@tinymceplominoform/action_form';
+            var elementIdName = 'actionid';
+        }
+        else if (elementType === "subform") {
+            var elementClass = 'plominoSubformClass';
+            var elementEditionPage = '@@tinymceplominoform/subform_form';
+            var elementIdName = 'subformid';
+        }
+        else if (elementType === "hidewhen") {
+            var elementClass = 'plominoHidewhenClass';
+            var elementEditionPage = '@@tinymceplominoform/hidewhen_form';
+            var elementIdName = 'hidewhenid';
+        }
+        else if (elementType === "cache") {
+            var elementClass = 'plominoCacheClass';
+            var elementEditionPage = '@@tinymceplominoform/cache_form';
+            var elementIdName = 'cacheid';
+        }
+        else
+            return;
+
+        // Find the element id
+        // Select the parent node of the selection
+        var selection = ed.selection.getNode();
+        // If the node is a <span class="plominoFieldClass"/>, select all its content
+        if (tinymce.DOM.hasClass(selection, elementClass))
+        {
+            ed.selection.select(selection);
+            var elementId = selection.firstChild.nodeValue;
+
+            // hide-when and cache zones start with start:id and finish with end:id
+            if (elementType === "hidewhen" || elementType === "cache")
+            {
+                var splittedId = elementId.split(':');
+                if (splittedId.length > 1)
+                    elementId = splittedId[1];
+            }
+        }
+        else if (elementType !== "hidewhen" && elementType !== "cache")
+        {
+            // If the selection contains a <span class="plominoFieldClass"/>, select all its content
+            nodes = tinymce.DOM.select('span.' + elementClass, selection);
+            if (nodes.length > 0)
+            {
+                // Search if a node in the found nodes belongs to the selection
+                for (var i = 0; i < nodes.length; i++)
+                {
+                    if (ed.selection.getContent().indexOf(tinymce.DOM.getOuterHTML(nodes[i])) != -1)
+                    {
+                        var node = nodes[i];
+                        break;
+                    }
+                }
+
+                // If a node is found, select it
+                if (node)
+                {
+                    ed.selection.select(node);
+                    var elementId = node.firstChild.nodeValue;
+                }
+                // Else, keep the selection
+                else
+                    var elementId = ed.selection.getContent();
+            }
+
+            // Else, keep the selection
+            else
+                var elementId = ed.selection.getContent();
+        }
+        else
+        {
+            var elementId = '';
+        }
+
+        var base_url = $('body').attr('data-base-url');
+
+        ed.windowManager.open({
+            url : base_url + '/' + elementEditionPage + '?' + elementIdName + '=' + elementId,
+            width : 600 + parseInt(ed.getLang('plomino_tinymce.delta_width', 0)),
+            height : 400 + parseInt(ed.getLang('plomino_tinymce.delta_height', 0)),
+            inline : 1
+        }, {
+            plugin_url : url,
+        });
+    };
+})();
---
+++ src/Products/CMFPlomino/browser/tinymce.py
@@ -0,0 +1,433 @@
+from zope import component, interface
+
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from Products.CMFPlomino.config import FIELD_MODES
+from Products.CMFPlomino.contents.field import get_field_types
+from Products.CMFPlomino.contents.action import ACTION_TYPES, ACTION_DISPLAY
+from Products.CMFPlomino.contents.action import PlominoAction
+from Products.CMFPlomino.contents.field import PlominoField
+#from Products.CMFPlomino.PlominoHidewhen import PlominoHidewhen
+#from Products.CMFPlomino.PlominoCache import PlominoCache
+
+
+class ITinyMCEPlominoFormView(interface.Interface):
+    """Marker interface"""
+
+
+class TinyMCEPlominoFormView(BrowserView):
+    """
+    """
+    interface.implements(ITinyMCEPlominoFormView)
+    
+    #action_template = ViewPageTemplateFile('templates/tinymce/action.pt')
+    def action_form(self, **params):
+        """."""
+        params = {}
+        return self.action_template(**params)
+    
+    #cache_template = ViewPageTemplateFile('templates/tinymce/cache.pt')
+    def cache_form(self, **params):
+        """."""
+        params = {}
+        return self.cache_template(**params)
+    
+    field_template = ViewPageTemplateFile('templates/tinymce/field.pt')
+    def field_form(self, **params):
+        """."""
+        params = {}
+        return self.field_template(**params)
+    
+    #hidewhen_template = ViewPageTemplateFile('templates/tinymce/hidewhen.pt')
+    def hidewhen_form(self, **params):
+        """."""
+        params = {}
+        return self.hidewhen_template(**params)
+    
+    #subform_template = ViewPageTemplateFile('templates/tinymce/subform.pt')
+    def subform_form(self, **params):
+        """."""
+        params = {}
+        return self.subform_template(**params)
+    
+    error_template = ViewPageTemplateFile('templates/tinymce/error.pt')
+    def error_page(self, **params):
+        """."""
+        params = {}
+        return self.error_template(**params)
+    
+    valid_template = ViewPageTemplateFile('templates/tinymce/valid.pt')
+    def valid_page(self, **params):
+        """."""
+        params = {}
+        return self.valid_template(**params)
+
+
+class PlominoForm(object):
+    """
+    """
+    action_template = ViewPageTemplateFile('templates/tinymce/action.pt')
+
+    def __init__(self, context, request):
+        """Initialize adapter."""
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        """
+        """
+        return self
+
+    def getFieldTypes(self):
+        """Return a list of possible types for a field.
+        """
+        fieldtypes = [(pair[0], pair[1][0]) for pair in get_field_types().items()]
+        fieldtypes.sort(key=lambda f:f[1])
+        return fieldtypes
+
+
+    def getFieldModes(self):
+        """Return a list of possible modes for a field.
+        """
+        return FIELD_MODES
+
+    def getField(self):
+        """Return a field from the request, or the first field if empty, or None if the specified field doesn't exist.
+        """
+        fieldid = self.request.get("fieldid", None)
+        if fieldid:
+            field = getattr(self.context, fieldid, None)
+            if isinstance(field, PlominoField):
+                return field
+            else:
+                return None
+
+        fieldsList = self.context.getFormFields()
+        if len(fieldsList) > 0:
+            return fieldsList[0]
+        else:
+            return None
+
+    def getFieldProperties(self):
+        """Return properties of an action, or , if no action is given, properties filled with default values.
+        """
+        field = self.getField()
+        if field:
+            return {'fieldType': field.FieldType,
+                    'fieldMode': field.FieldMode,
+                    'formula': field.Formula
+                    }
+        else:
+             return {'fieldType': 'TEXT',
+                    'fieldMode': 'EDITABLE',
+                    'formula': ''
+                    }
+
+    def addField(self):
+        """Add a field to the form.
+        """
+        fieldid = self.request.get("fieldid", None)
+        fieldtype = self.request.get("fieldtype", "TEXT")
+        fieldmode = self.request.get("fieldmode", "EDITABLE")
+        fieldformula = self.request.get("fieldformula", "")
+
+        # self.context is the current form
+        if fieldid:
+            if not hasattr(self.context, fieldid):
+                self.context.invokeFactory('PlominoField', Title=fieldid, id=fieldid, FieldType=fieldtype, FieldMode=fieldmode)
+                field = self.context.aq_inner.getFormField(fieldid)
+                field.setFormula(fieldformula)
+                field.setTitle(fieldid)
+                field.at_post_create_script()
+
+                self.request.RESPONSE.redirect(self.context.absolute_url() + "/@@tinymceplominoform/valid_page?type=field&value=" + fieldid + "&fieldurl=" + "/".join(field.getPhysicalPath()))
+
+            else:
+                self.request.RESPONSE.redirect(self.context.absolute_url() + "/@@tinymceplominoform/error_page?error=object_exists")
+
+        else:
+            self.request.RESPONSE.redirect(self.context.absolute_url() + "/@@tinymceplominoform/error_page?error=no_field")
+
+
+    def getActions(self):
+        """Returns a sorted list of actions
+        """
+        actions = self.context.aq_inner.getActions(None, False)
+        actions = [a[0] for a in actions]
+        actions.sort(key=lambda elt: elt.id.lower())
+        return actions
+
+
+    def getActionTypes(self):
+        """Return a list of possible types for an action.
+        """
+        return ACTION_TYPES
+
+    def getActionDisplay(self):
+        """Return a list of possible displays for an action.
+        """
+        return ACTION_DISPLAY
+
+    def getAction(self):
+        """Return an action from the request, or the first action if empty, or None if the specified action doesn't exist.
+        """
+        actionid = self.request.get("actionid", None)
+        if actionid:
+            action = getattr(self.context, actionid, None)
+            if isinstance(action, PlominoAction):
+                return action
+            else:
+                return None
+
+        actionsList = self.context.getActions(None, False)
+        if len(actionsList) > 0:
+            return actionsList[0][0]
+        else:
+            return None
+
+    def getActionProperties(self):
+        """Return properties of an action, or , if no action is given, properties filled with default values.
+        """
+        action = self.getAction()
+        if action:
+            return {'title': action.Title(),
+                    'actionType': action.ActionType,
+                    'actionDisplay': action.ActionDisplay,
+                    'content': action.Content,
+                    'hideWhen': action.Hidewhen,
+                    'inActionBar': action.InActionBar
+                    }
+        else:
+             return {'title': '',
+                     'actionType': 'OPENFORM',
+                     'actionDisplay': 'LINK',
+                     'content': '',
+                     'hideWhen': '',
+                     'inActionBar': False
+                     }
+
+    def addAction(self):
+        """ Add an action to the form.
+        """
+        actionid = self.request.get("actionid", None)
+        title = self.request.get("actiontitle", actionid)
+        actionType = self.request.get("actiontype", 'OPENFORM')
+        actionDisplay = self.request.get("actiondisplay", 'LINK')
+        content = self.request.get("actioncontent", '')
+        hideWhen = self.request.get("actionhidewhen", '')
+        inActionBar = self.request.get("actioninactionbar", None) == 'on'
+
+        # self.context is the current form
+        if actionid:
+            if not hasattr(self.context, actionid):
+                self.context.invokeFactory('PlominoAction', Title=title, id=actionid, ActionType=actionType, ActionDisplay=actionDisplay, Content=content, Hidewhen=hideWhen, InActionBar=inActionBar)
+                action = getattr(self.context.aq_inner, actionid)
+                action.setTitle(title)
+                #action = getattr(self.context.aq_parent.aq_parent, actionid)
+                #action.at_post_edit_script()
+
+                self.request.RESPONSE.redirect(self.context.absolute_url() + "/@@tinymceplominoform/valid_page?type=action&value=" + actionid)
+
+            else:
+                self.request.RESPONSE.redirect(self.context.absolute_url() + "/@@tinymceplominoform/error_page?error=object_exists")
+
+        else:
+            self.request.RESPONSE.redirect(self.context.absolute_url() + "/@@tinymceplominoform/error_page?error=no_action")
+
+    def getSubForms(self):
+        """Returns a list of forms from the parent database, without the current form
+        """
+        form = self.context.aq_inner
+        subforms = form.getParentDatabase().getForms()
+        subforms.remove(form)
+        subforms.sort(key=lambda elt: elt.id.lower())
+        return subforms
+
+    def getHidewhenFormulas(self):
+        """Returns a sorted list of Hide-when
+        """
+        hw = self.context.aq_inner.getHidewhenFormulas()
+        hw.sort(key=lambda elt: elt.id.lower())
+        return hw
+
+    def getHidewhen(self):
+        """Returns a hide-when formula from the request, or the first hide-when formula if empty, or None if the specified hide-when doesn't exist.
+        """
+        hidewhenid = self.request.get("hidewhenid", None)
+
+        if self.request.get("create", False):
+            return None
+
+        if hidewhenid:
+            hidewhen = getattr(self.context, hidewhenid, None)
+            if isinstance(hidewhen, PlominoHidewhen):
+                return hidewhen
+            else:
+                return None
+
+        hidewhenList = self.context.getHidewhenFormulas()
+        if len(hidewhenList) > 0:
+            return hidewhenList[0]
+        else:
+            return None
+
+    def getHidewhenProperties(self):
+        """Returns properties of a hide-when formula, or, if no hide-when formula is given, properties filled with default values.
+        """
+        hidewhen = self.getHidewhen()
+        if hidewhen:
+            return { 'formula': hidewhen.Formula, 'isdynamichidewhen': hidewhen.isDynamicHidewhen }
+        else:
+             return { 'formula': '', 'isdynamichidewhen': False }
+
+    def addHidewhen(self):
+        """ Add a hide-when to the form.
+        """
+        hidewhenid = self.request.get("hidewhenid", None)
+        hidewhenformula = self.request.get("hidewhenformula", '')
+        hidewhentype = self.request.get("hidewhentype", 'static')
+
+        # self.context is the current form
+        if hidewhenid:
+            if not hasattr(self.context, hidewhenid):
+                self.context.invokeFactory('PlominoHidewhen', Title=hidewhenid, id=hidewhenid, Formula=hidewhenformula, isDynamicHidewhen=hidewhentype=='dynamic')
+                hidewhen = getattr(self.context.aq_inner, hidewhenid)
+                hidewhen.setTitle(hidewhenid)
+#                hidewhen.at_post_edit_script()
+
+                self.request.RESPONSE.redirect(self.context.absolute_url() + "/@@tinymceplominoform/valid_page?type=hidewhen&value=" + hidewhenid)
+
+            else:
+                self.request.RESPONSE.redirect(self.context.absolute_url() + "/@@tinymceplominoform/error_page?error=object_exists")
+
+        else:
+            self.request.RESPONSE.redirect(self.context.absolute_url() + "/@@tinymceplominoform/error_page?error=no_hidewhen")
+
+    #PLOMINO CACHE
+    def getCacheFormulas(self):
+        """Returns a sorted list of caches
+        """
+        hw = self.context.aq_inner.getCacheFormulas()
+        hw.sort(key=lambda elt: elt.id.lower())
+        return hw
+
+    def getCacheFormula(self):
+        """Returns a cache formula from the request, or the first cache formula if empty, or None if the specified cache doesn't exist.
+        """
+        cacheid = self.request.get("cacheid", None)
+
+        if self.request.get("create", False):
+            return None
+
+        if cacheid:
+            cache = getattr(self.context, cacheid, None)
+            if isinstance(cache, PlominoCache):
+                return cache
+            else:
+                return None
+
+        cacheList = self.context.getCacheFormulas()
+        if len(cacheList) > 0:
+            return cacheList[0]
+        else:
+            return None
+
+    def getCacheProperties(self):
+        """Returns properties of a cache formula, or, if no cache formula is given, properties filled with default values.
+        """
+        cache = self.getCacheFormula()
+        if cache:
+            return { 'formula': cache.Formula}
+        else:
+             return { 'formula': ''}
+
+    def addCache(self):
+        """ Add a cache to the form.
+        """
+        cacheid = self.request.get("cacheid", None)
+        cacheformula = self.request.get("cacheformula", '')
+
+        # self.context is the current form
+        if cacheid:
+            if not hasattr(self.context, cacheid):
+                self.context.invokeFactory('PlominoCache', Title=cacheid, id=cacheid, Formula=cacheformula)
+                cache = getattr(self.context.aq_inner, cacheid)
+                cache.setTitle(cacheid)
+
+                self.request.RESPONSE.redirect(self.context.absolute_url() + "/@@tinymceplominoform/valid_page?type=cache&value=" + cacheid)
+
+            else:
+                self.request.RESPONSE.redirect(self.context.absolute_url() + "/@@tinymceplominoform/error_page?error=object_exists")
+
+        else:
+            self.request.RESPONSE.redirect(self.context.absolute_url() + "/@@tinymceplominoform/error_page?error=no_cache")
+
+
+class PlominoField(object):
+    """
+    """
+
+    def __init__(self, context, request):
+        """Initialize adapter."""
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        """
+        """
+        return self
+
+    def setFieldProperties(self):
+        """Set field properties to their new values.
+        """
+
+        fieldtype = self.request.get("fieldtype", 'TEXT')
+        fieldmode = self.request.get("fieldmode", 'EDITABLE')
+        fieldformula = self.request.get("fieldformula", '')
+
+        # self.context is the current field
+        self.context.setFieldType(fieldtype)
+        self.context.setFieldMode(fieldmode)
+        self.context.setFormula(fieldformula)
+        self.context.aq_inner.at_post_edit_script()
+
+        self.request.RESPONSE.redirect(self.context.absolute_url() + "/../@@tinymceplominoform/valid_page?type=field&value=" + self.context.id)
+
+
+class PlominoAction(object):
+    """
+    """
+
+    def __init__(self, context, request):
+        """Initialize adapter."""
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        """
+        """
+        return self
+
+    def setActionProperties(self):
+        """Set field properties to their new values.
+        """
+
+        title = self.request.get("actiontitle", self.context.id)
+        actionType = self.request.get("actiontype", 'OPENFORM')
+        actionDisplay = self.request.get("actiondisplay", 'LINK')
+        content = self.request.get("actioncontent", '')
+        hideWhen = self.request.get("actionhidewhen", '')
+        inActionBar = self.request.get("actioninactionbar", None) == 'on'
+
+        # self.context is the current field
+        self.context.setTitle(title)
+        self.context.setActionType(actionType)
+        self.context.setActionDisplay(actionDisplay)
+        self.context.setContent(content)
+        self.context.setHidewhen(hideWhen)
+        self.context.setInActionBar(inActionBar)
+
+        self.context.aq_inner.at_post_edit_script()
+
+        self.request.RESPONSE.redirect(self.context.absolute_url() + "/../@@tinymceplominoform/valid_page?type=action&value=" + self.context.id)
