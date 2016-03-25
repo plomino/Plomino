38c504b8cc89eea5619bde6a72a3ab61c012e669
Eric BREHAULT <ebrehault@gmail.com>
2016-03-25 14:07:34 +0100

    update js bundle

--- src/Products/CMFPlomino/browser/static/js/plomino-compiled.js
+++ src/Products/CMFPlomino/browser/static/js/plomino-compiled.js
@@ -6,6 +6,7 @@ require([
     
     var PlominoFormula = Base.extend({
         name: 'plominoformula',
+        parser: 'mockup',
         trigger: '.plomino-formula',
         defaults: {},
         init: function() {
@@ -35,6 +36,7 @@ require([
     
     var Table = Base.extend({
         name: 'plominotable',
+        parser: 'mockup',
         trigger: '.plomino-table',
         defaults: {},
         init: function() {
@@ -114,12 +116,78 @@ define("plominotable", function(){});
 
 require([
     'jquery',
+    'pat-base'
+], function($, Base) {
+    
+    var Dynamic = Base.extend({
+        name: 'plominodynamic',
+        parser: 'mockup',
+        trigger: '#plomino_form',
+        defaults: {},
+        init: function() {
+            var self = this;
+            this.$el.find(':input').change(function(e) {
+                self.refresh(e.target);
+            });
+        },
+        refresh: function(field) {
+            var self = this;
+            var data = self.getCurrentInputs();
+            if(self.options.docid) {
+                data._docid = self.options.docid;
+            }
+            data._hidewhens = self.getHidewhens();
+            data._validation = field.id;
+            $.post(self.options.url + '/dynamic_evaluation',
+                data,
+                function(response) {
+                    self.applyHidewhens(response.hidewhens);
+                },
+                'json');
+        },
+        getCurrentInputs: function() {
+            var data = {};
+            var inputs = $("form").serialize().split("&");
+            for(var key in inputs) {
+                data[inputs[key].split("=")[0]] = inputs[key].split("=")[1];
+            }
+            return data;
+        },
+        getHidewhens: function() {
+            var self = this;
+            var hidewhens = [];
+            self.$el.find('.plomino-hidewhen').each(function(i, el) {
+                hidewhens.push($(el).attr('data-hidewhen'));
+            });
+            return hidewhens;
+        },
+        applyHidewhens: function(hidewhens) {
+            var self = this;
+            for(var i=0; i<hidewhens.length; i++) {
+                var hwid = hidewhens[i][0];
+                var status = hidewhens[i][1];
+                var area = self.$el.find('.plomino-hidewhen[data-hidewhen="'+hwid+'"]');
+                if(status) {
+                    area.hide();
+                } else {
+                    area.show();
+                }
+            }
+        }
+    });
+    return Dynamic;
+});
+define("plominodynamic", function(){});
+
+require([
+    'jquery',
     'pat-base',
     'mockup-patterns-tree'
 ], function($, Base, Tree) {
     
     var PlominoDesign = Base.extend({
         name: 'plominodesign',
+        parser: 'mockup',
         trigger: '.plomino-design',
         defaults: {},
         init: function() {
@@ -237,76 +305,13 @@ define("plominodesign", function(){});
 
 require([
     'jquery',
-    'pat-base'
-], function($, Base) {
-    
-    var Dynamic = Base.extend({
-        name: 'plominodynamic',
-        trigger: '#plomino_form',
-        defaults: {},
-        init: function() {
-            var self = this;
-            this.$el.find(':input').change(function(e) {
-                self.refresh(e.target);
-            });
-        },
-        refresh: function(field) {
-            var self = this;
-            var data = self.getCurrentInputs();
-            if(self.options.docid) {
-                data._docid = self.options.docid;
-            }
-            data._hidewhens = self.getHidewhens();
-            data._validation = field.id;
-            $.post(self.options.url + '/dynamic_evaluation',
-                data,
-                function(response) {
-                    self.applyHidewhens(response.hidewhens);
-                },
-                'json');
-        },
-        getCurrentInputs: function() {
-            var data = {};
-            var inputs = $("form").serialize().split("&");
-            for(var key in inputs) {
-                data[inputs[key].split("=")[0]] = inputs[key].split("=")[1];
-            }
-            return data;
-        },
-        getHidewhens: function() {
-            var self = this;
-            var hidewhens = [];
-            self.$el.find('.plomino-hidewhen').each(function(i, el) {
-                hidewhens.push($(el).attr('data-hidewhen'));
-            });
-            return hidewhens;
-        },
-        applyHidewhens: function(hidewhens) {
-            var self = this;
-            for(var i=0; i<hidewhens.length; i++) {
-                var hwid = hidewhens[i][0];
-                var status = hidewhens[i][1];
-                var area = self.$el.find('.plomino-hidewhen[data-hidewhen="'+hwid+'"]');
-                if(status) {
-                    area.hide();
-                } else {
-                    area.show();
-                }
-            }
-        }
-    });
-    return Dynamic;
-});
-define("plominodynamic", function(){});
-
-require([
-    'jquery',
     'pat-base',
     'mockup-patterns-modal'
 ], function($, Base, Modal) {
     
     var DataGrid = Base.extend({
         name: 'plominodatagrid',
+        parser: 'mockup',
         trigger: '.plomino-datagrid',
         defaults: {},
         init: function() {
