a89b051960aec6bef1f75245746e0ce2d93f00b6
Maxime Bouveron <maxime.bouveron@gmail.com>
2016-04-15 16:57:37 +0200

    added tinyMCE

--- ide/app/app.component.ts
+++ ide/app/app.component.ts
@@ -2,17 +2,19 @@ import {Component, ViewChild} from 'angular2/core';
 import {TreeComponent} from './tree-view/tree.component';
 import {MODAL_DIRECTIVES, ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
 import {TAB_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
+import {TinyMCEComponent} from './tiny-mce.component';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.css'],
-    directives: [TreeComponent, MODAL_DIRECTIVES, TAB_DIRECTIVES]
+    directives: [TreeComponent, MODAL_DIRECTIVES, TAB_DIRECTIVES, TinyMCEComponent]
 })
 export class AppComponent {
     @ViewChild('modal')
     modal: ModalComponent;
 
+    selectedEditor: string;
     tabs: Array<any> = [];
     newName: string;
 
@@ -25,7 +27,7 @@ export class AppComponent {
         this.tabs.push(newtab);
     }
     onModalClose() {
-        this.tabs.push({ title: this.newName, content: 'I am the content of ' + this.newName });
+        this.tabs.push({ title: this.newName, content: 'content of '+this.newName });
     }
     onTabClose(tab: any) {
         this.tabs.splice(this.tabs.indexOf(tab), 1);
--- ide/app/tiny-mce.component.ts
+++ ide/app/tiny-mce.component.ts
@@ -1,8 +1,23 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit, Input} from 'angular2/core';
+
+declare var tinymce: any;
 
 @Component({
-    selector: 'my-tiny-mce'
+    selector: 'my-tiny-mce',
+    template: '<textarea class="tinymce-wrap"></textarea>'
 })
 export class TinyMCEComponent {
-    
+    @Input() content: string;
+
+    ngOnInit(){
+        tinymce.init({
+            selector:'.tinymce-wrap',
+            plugins: ["code"],
+		    menubar: true,
+            height : "354"
+        });
+        tinymce.activeEditor.setContent(this.content);
+
+        console.log(this.content);
+    }
 }
