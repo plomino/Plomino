195c58539bbbb2d50008a0e8bbea77697d2654d2
Eric BREHAULT <ebrehault@gmail.com>
2016-06-26 17:46:12 +0200

    fix field creation from IDE

--- src/Products/CMFPlomino/browser/form.py
+++ src/Products/CMFPlomino/browser/form.py
@@ -1,5 +1,9 @@
+from jsonutil import jsonutil as json
+import plone.protect.interfaces
+from plone import api
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.interface import alsoProvides
 
 
 class FormView(BrowserView):
@@ -18,3 +22,20 @@ class FormView(BrowserView):
 
     def openbareform(self):
         return self.bare_template()
+
+    def addField(self):
+        # specific field settings are managed as instance behaviors (using
+        # collective.instancebehavior), but it is not supported by
+        # plone.restapi, so we implement our own endpoint to create fields.
+        self.request.RESPONSE.setHeader(
+            'content-type', 'text/plain; charset=utf-8')
+        if self.request.method == "POST":
+            alsoProvides(
+                self.request, plone.protect.interfaces.IDisableCSRFProtection)
+            data = json.loads(self.request.BODY)
+            newfield = api.content.create(
+                container=self.context,
+                type="PlominoField",
+                title=data['title'],
+            )
+            return json.dumps({'created': newfield.id})
