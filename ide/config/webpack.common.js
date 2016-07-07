caef7ec5cc3b3399ef99423d82a52bce3870d4f4
Eric BREHAULT <ebrehault@gmail.com>
2016-07-07 19:17:14 +0200

    use local bootstrap

--- ide/config/webpack.common.js
+++ ide/config/webpack.common.js
@@ -42,7 +42,9 @@ module.exports = {
   },
   plugins: [
     new copyWebpackPlugin([
-        { from: 'node_modules/tinymce/skins', to: 'skins' }
+        { from: 'node_modules/tinymce/skins', to: 'skins' },
+        { from: 'node_modules/bootstrap/dist/css/bootstrap.min.css', to: 'bootstrap/css' },
+        { from: 'node_modules/bootstrap/dist/fonts', to: 'bootstrap/fonts' }
     ]),
     new webpack.optimize.CommonsChunkPlugin({
         name: ['main', 'vendor']
