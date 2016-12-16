f2b16257ae9c07ffa243d52abb6b81ed4ec229b5
Daniel Marks <dmarks@pretaweb.com>
2016-12-16 11:43:41 +1030

    Add a new palette component for controlling the IDE

---
+++ ide/app/palette-view/dbsettings.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'plomino-palette-dbsettings',
+    template: require('./dbsettings.component.html'),
+    directives: [],
+    providers: []
+})
+
+export class DBSettingsComponent {
+
+}
---
+++ ide/app/palette-view/fieldsettings.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'plomino-palette-fieldsettings',
+    template: require('./fieldsettings.component.html'),
+    directives: [],
+    providers: []
+})
+
+export class FieldSettingsComponent {
+
+}
