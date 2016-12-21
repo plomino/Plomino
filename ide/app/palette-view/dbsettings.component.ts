40fc49dd5a4d474bf88cc93c6769049e143f9ba1
Daniel Marks <dmarks@pretaweb.com>
2016-12-21 17:21:05 +1030

    Add a custom Pipe to allow trusted HTML from Plone; Make use of Object Service to display the DB Edit Form

--- ide/app/palette-view/dbsettings.component.ts
+++ ide/app/palette-view/dbsettings.component.ts
@@ -1,12 +1,48 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { ObjService }                   from '../services/obj.service';
+import { PloneHtml } from '../custom.pipes';
+
+declare var $: any;
 
 @Component({
     selector: 'plomino-palette-dbsettings',
     template: require('./dbsettings.component.html'),
     directives: [],
-    providers: []
+    providers: [ObjService],
+    pipes: [PloneHtml],
 })
 
 export class DBSettingsComponent {
 
+    @ViewChild('dbform') el:ElementRef;
+
+    dbForm: any;
+
+    constructor(private _objService: ObjService) { }
+
+    saved() {
+        // this.el is the div that contains the Edit Form
+        // We want to seralize the data in the form, submit it to the form
+        // action. If the response is <div class="ajax_success">, we re-fetch
+        // the form. Otherwise we update the displayed form with the response
+        // (which may have validation failures etc.) 
+        console.log(this.el.nativeElement);
+    }
+
+    ngOnInit() {
+        // Fetch the DB form from the object service
+        this.getDBForm();
+    }
+
+    getDBForm() {
+        this._objService.getDB()
+            .subscribe(
+                html => { this.dbForm = html },
+                err => console.error(err)
+            );
+    }
+
 }
+
+
