6604d252480366292a98f7141852469e53c816e0
Daniel Marks <dmarks@pretaweb.com>
2016-12-22 16:43:25 +1030

    Add buttons in the palette

--- ide/app/palette-view/add.component.ts
+++ ide/app/palette-view/add.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'plomino-palette-add',
@@ -8,5 +8,56 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 
 export class AddComponent {
+
+    addableComponents: Array<any> = [];
+
+    ngOnInit() {
+        // Set up the addable components
+        this.addableComponents = [
+            {title: 'DB', components: [
+                {title: 'Form', icon: 'th-list', type: 'form', addable: true},
+                {title: 'View', icon: 'list-alt', type: 'view', addable: true},
+                ]
+            },
+            {title: 'Form', components: [
+                {title: 'Field', icon: 'tasks', type: 'field', addable: false},
+                {title: 'Hide When', icon: 'sunglasses', type: 'hidewhen', addable: false},
+                {title: 'Action', icon: 'cog', type: 'action', addable: false},
+                ]
+            },
+            {title: 'View', components: [
+                {title: 'Column', icon: 'stats', type: 'column', addable: false},
+                {title: 'Action', icon: 'cog', type: 'action', addable: false},
+                ]
+            }
+        ];
+    }
+
+    add(type: any) {
+        // console.log(event.target.id);
+        // XXX: Handle the adding of components
+
+        // XXX: Remove this later
+        // This isn't real. Controlling the state of the buttons needs to be done
+        // By listening to the event that handles the currently selected item 
+        if (type == 'form') {
+            for (let component of this.addableComponents[1]['components']) {
+                component.addable = true;
+            }
+            for (let component of this.addableComponents[2]['components']) {
+                component.addable = false;
+            }
+        } else if (type == 'view') {
+            for (let component of this.addableComponents[2]['components']) {
+                component.addable = true;
+            }
+            for (let component of this.addableComponents[1]['components']) {
+                component.addable = false;
+            }
+        }
+    }
+
+    // When a Form or View is selected, adjust the addable state of the
+    // relevant buttons
 
 }
