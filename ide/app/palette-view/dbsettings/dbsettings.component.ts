02d73e4f14122377325b3fd15d66f7646cf1e3b1
Dima Panasyuk <dima_panasyuk@ukr.net>
2016-12-30 13:02:18 +0200

    Fixed bug with settings autoupdate after save

--- ide/app/palette-view/dbsettings/dbsettings.component.ts
+++ ide/app/palette-view/dbsettings/dbsettings.component.ts
@@ -53,16 +53,14 @@ export class DBSettingsComponent {
         formData.append('form.buttons.save', 'Save');
         
         this.objService.submitDB(formData)
-            .flatMap((responseHtml) => {
-                let $responseHtml = $(responseHtml);
-                if (!!$responseHtml.find('dl.error')) {
+            .flatMap((responseHtml: string) => {
+                if (responseHtml.indexOf('dl.error') > -1) {
                     return Observable.of(responseHtml);
                 } else {
                     return this.objService.getDB();
                 }
             })
             .subscribe(responseHtml => {
-                console.info(responseHtml);
                 this.dbForm = responseHtml;
                 this.changeDetector.markForCheck();
             }, err => { 
