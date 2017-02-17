5e6e5ad5814d3fe201576d13b9ba67c0e06eb4f2
Ivan Maslov <cryptox3@gmail.com>
2017-02-18 00:06:29 +0500

    fixed observable

--- ide/app/services/widget.service.ts
+++ ide/app/services/widget.service.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@angular/core';
 
 import { Http, Response } from '@angular/http';
 
-import { Observable } from 'rxjs/Rx';
-
 import 'jquery';
 declare let $: any;
 import 'lodash';
