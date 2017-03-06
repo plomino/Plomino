ce8a5ab6d2c1c6575199b1645fc34a13c2ac29f4
Ivan Maslov <cryptox3@gmail.com>
2017-03-06 13:40:17 +0400

    loader

---
+++ ide/app/services/application-loader.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class PlominoApplicationLoaderService {
+
+  private shouldBeLoaded: string[] = ['app.component', 'material'];
+
+  constructor() {
+    window['materialPromise'].then(() => {
+      this.markLoaded('material');
+    });
+  }
+
+  markLoaded(key: string) {
+    this.shouldBeLoaded = this.shouldBeLoaded.filter((e) => e !== key);
+
+    if (!this.shouldBeLoaded.length) {
+      this.onLoad();
+    }
+  }
+
+  private onLoad() {
+    $('#application-loader').remove();
+  }
+}
