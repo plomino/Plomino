6882d557f8416ffe4e15a8c230608d277d5e23f0
Dylan Jay <software@pretaweb.com>
2017-05-02 15:27:24 +0700

    latest build of JS

--- src/Products/CMFPlomino/browser/static/ide/2.chunk.js
+++ src/Products/CMFPlomino/browser/static/ide/2.chunk.js
@@ -1,1 +1,54 @@
-webpackJsonp([2,0],{150:function(t,e,r){var n,o;n=[r(4),r(87),r(248),r(151)],o=function(t,e,r,n){"use strict";var o=n.getLogger("Patternslib Base"),i=function(t,o,i){var a=this.prototype.name,s=n.getLogger("pat."+a),p=t.data("pattern-"+a);if(void 0===p&&e.patterns[a]){try{o="mockup"===this.prototype.parser?r.getOptions(t,a,o):o,p=new e.patterns[a](t,o,i)}catch(t){s.error("Failed while initializing '"+a+"' pattern.",t)}t.data("pattern-"+a,p)}return p},a=function(e,r,n){this.$el=e,this.options=t.extend(!0,{},this.defaults||{},r||{}),this.init(e,r,n),this.emit("init")};return a.prototype={constructor:a,on:function(t,e){this.$el.on(t+"."+this.name+".patterns",e)},emit:function(t,e){void 0===e&&(e=[]),this.$el.trigger(t+"."+this.name+".patterns",e)}},a.extend=function(r){var n,s=this;if(!r)throw new Error("Pattern configuration properties required when calling Base.extend");n=r.hasOwnProperty("constructor")?r.constructor:function(){s.apply(this,arguments)},n.extend=a.extend,n.init=i,n.jquery_plugin=!0,n.trigger=r.trigger;var p=function(){this.constructor=n};return p.prototype=s.prototype,n.prototype=new p,t.extend(!0,n.prototype,r),n.__super__=s.prototype,r.name?r.trigger?e.register(n,r.name):o.warn("The pattern '"+r.name+"' does not have a trigger attribute, it will not be registered."):o.warn("This pattern without a name attribute will not be registered!"),n},a}.apply(e,n),!(void 0!==o&&(t.exports=o))},248:function(t,e,r){var n,o;n=[r(4)],o=function(t){"use strict";var e={getOptions:function e(r,n,o){o=o||{},0===r.length||t.nodeName(r[0],"body")||(o=e(r.parent(),n,o));var i={};if(0!==r.length&&(i=r.data("pat-"+n),i&&"string"==typeof i)){var a={};t.each(i.split(";"),function(t,e){e=e.split(":"),e.reverse();var r=e.pop();r=r.replace(/^\s+|\s+$/g,""),e.reverse();var n=e.join(":");n=n.replace(/^\s+|\s+$/g,""),a[r]=n}),i=a}return t.extend(!0,{},o,i)}};return e}.apply(e,n),!(void 0!==o&&(t.exports=o))},719:function(t,e,r){var n,o;n=[r(4),r(40),r(367),r(357)],o=function(t,e,r,n){"use strict";var o=e.extend({name:"sortable",trigger:".pat-sortable",defaults:{selector:"li",dragClass:"item-dragging",cloneClass:"dragging",createDragItem:function(t,e){return e.clone().addClass(t.options.cloneClass).css({opacity:.75,position:"absolute"}).appendTo(document.body)},drop:null},init:function(){var e=this,r=0;e.$el.find(e.options.selector).drag("start",function(o,i){var a=this,s=t(this);return t(a).addClass(e.options.dragClass),n({tolerance:function(r,n,o){if(0!==t(o.elem).closest(e.$el).length){var i=r.pageY>o.top+o.height/2;return t.data(o.elem,"drop+reorder",i?"insertAfter":"insertBefore"),this.contains(o,[r.pageX,r.pageY])}}}),r=s.index(),e.options.createDragItem(e,s)}).drag(function(e,r){t(r.proxy).css({top:r.offsetY,left:r.offsetX});var n=r.drop[0],o=t.data(n||{},"drop+reorder");o&&n&&(n!=r.current||o!=r.method)&&(t(this)[o](n),r.current=n,r.method=o,r.update())}).drag("end",function(n,o){var i=t(this);i.removeClass(e.options.dragClass),t(o.proxy).remove(),e.options.drop&&e.options.drop(i,i.index()-r)}).drop("init",function(t,e){return this!=e.drag})}});return o}.apply(e,n),!(void 0!==o&&(t.exports=o))}});
+webpackJsonp([2],{/***/
+1292:/***/
+function(module,exports,__webpack_require__){var __WEBPACK_AMD_DEFINE_ARRAY__,__WEBPACK_AMD_DEFINE_RESULT__;__WEBPACK_AMD_DEFINE_ARRAY__=[__webpack_require__(553),__webpack_require__(1203),__webpack_require__(1293),__webpack_require__(1204)],__WEBPACK_AMD_DEFINE_RESULT__=function($,Registry,mockupParser,logger){"use strict";var log=logger.getLogger("Patternslib Base"),initBasePattern=function($el,options,trigger){var name=this.prototype.name,log=logger.getLogger("pat."+name),pattern=$el.data("pattern-"+name);if(void 0===pattern&&Registry.patterns[name]){try{options="mockup"===this.prototype.parser?mockupParser.getOptions($el,name,options):options,pattern=new Registry.patterns[name]($el,options,trigger)}catch(e){log.error("Failed while initializing '"+name+"' pattern.",e)}$el.data("pattern-"+name,pattern)}return pattern},Base=function($el,options,trigger){this.$el=$el,this.options=$.extend(!0,{},this.defaults||{},options||{}),this.init($el,options,trigger),this.emit("init")};return Base.prototype={constructor:Base,on:function(eventName,eventCallback){this.$el.on(eventName+"."+this.name+".patterns",eventCallback)},emit:function(eventName,args){
+// args should be a list
+void 0===args&&(args=[]),this.$el.trigger(eventName+"."+this.name+".patterns",args)}},Base.extend=function(patternProps){/* Helper function to correctly set up the prototype chain for new patterns.
+	        */
+var child,parent=this;
+// Check that the required configuration properties are given.
+if(!patternProps)throw new Error("Pattern configuration properties required when calling Base.extend");
+// The constructor function for the new subclass is either defined by you
+// (the "constructor" property in your `extend` definition), or defaulted
+// by us to simply call the parent's constructor.
+child=patternProps.hasOwnProperty("constructor")?patternProps.constructor:function(){parent.apply(this,arguments)},
+// Allow patterns to be extended indefinitely
+child.extend=Base.extend,
+// Static properties required by the Patternslib registry 
+child.init=initBasePattern,child.jquery_plugin=!0,child.trigger=patternProps.trigger;
+// Set the prototype chain to inherit from `parent`, without calling
+// `parent`'s constructor function.
+var Surrogate=function(){this.constructor=child};
+// Add pattern's configuration properties (instance properties) to the subclass,
+// Set a convenience property in case the parent's prototype is needed
+// later.
+// Register the pattern in the Patternslib registry.
+return Surrogate.prototype=parent.prototype,child.prototype=new Surrogate,$.extend(!0,child.prototype,patternProps),child.__super__=parent.prototype,patternProps.name?patternProps.trigger?Registry.register(child,patternProps.name):log.warn("The pattern '"+patternProps.name+"' does not have a trigger attribute, it will not be registered."):log.warn("This pattern without a name attribute will not be registered!"),child},Base}.apply(exports,__WEBPACK_AMD_DEFINE_ARRAY__),/**
+	 * A Base pattern for creating scoped patterns. It's similar to Backbone's
+	 * Model class. The advantage of this approach is that each instance of a
+	 * pattern has its own local scope (closure).
+	 *
+	 * A new instance is created for each DOM element on which a pattern applies.
+	 *
+	 * You can assign values, such as $el, to `this` for an instance and they
+	 * will remain unique to that instance.
+	 *
+	 * Older Patternslib patterns on the other hand have a single global scope for
+	 * all DOM elements.
+	 */
+!(void 0!==__WEBPACK_AMD_DEFINE_RESULT__&&(module.exports=__WEBPACK_AMD_DEFINE_RESULT__))},/***/
+1293:/***/
+function(module,exports,__webpack_require__){var __WEBPACK_AMD_DEFINE_ARRAY__,__WEBPACK_AMD_DEFINE_RESULT__;__WEBPACK_AMD_DEFINE_ARRAY__=[__webpack_require__(553)],__WEBPACK_AMD_DEFINE_RESULT__=function($){"use strict";var parser={getOptions:function getOptions($el,patternName,options){/* This is the Mockup parser. An alternative parser for Patternslib
+	             * patterns.
+	             *
+	             * NOTE: Use of the Mockup parser is discouraged and is added here for
+	             * legacy support for the Plone Mockup project.
+	             *
+	             * It parses a DOM element for pattern configuration options.
+	             */
+options=options||{},
+// get options from parent element first, stop if element tag name is 'body'
+0===$el.length||$.nodeName($el[0],"body")||(options=getOptions($el.parent(),patternName,options));
+// collect all options from element
+var elOptions={};if(0!==$el.length&&(elOptions=$el.data("pat-"+patternName),elOptions&&"string"==typeof elOptions)){var tmpOptions={};$.each(elOptions.split(";"),function(i,item){item=item.split(":"),item.reverse();var key=item.pop();key=key.replace(/^\s+|\s+$/g,""),// trim
+item.reverse();var value=item.join(":");value=value.replace(/^\s+|\s+$/g,""),// trim
+tmpOptions[key]=value}),elOptions=tmpOptions}return $.extend(!0,{},options,elOptions)}};return parser}.apply(exports,__WEBPACK_AMD_DEFINE_ARRAY__),!(void 0!==__WEBPACK_AMD_DEFINE_RESULT__&&(module.exports=__WEBPACK_AMD_DEFINE_RESULT__))}});
