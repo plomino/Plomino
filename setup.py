ba964c479b83e353f4acc8d379b86dd41c209cf9
Dylan Jay <software@pretaweb.com>
2017-06-06 18:08:32 +0700

    stop using checkouts for Plone and plone.rest as these should now be right versions

--- setup.py
+++ setup.py
@@ -47,7 +47,9 @@ setup(
         'jsonutil',
         'collective.instancebehavior',
         'pyquery',
+        'plone.rest>=1.0a8',
         'plone.restapi>=1.0a11',
+        'Products.CMFPlone >= 5.0.7', #for tinymce 4.4.3 (mockup >= 2.4.0)
     ],
     extras_require={
         'test': [
