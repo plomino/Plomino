2d23ce4a42a387eff39f66aaad1ae915740102b5
Jon Pentland <jon@iomedia.co.uk>
2017-06-30 10:38:56 +0100

    Require plone.reset>=1.0a7

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'jsonutil',
         'collective.instancebehavior',
         'pyquery',
-        'plone.rest>=1.0a8',
+        'plone.rest>=1.0a7',
         'plone.restapi>=1.0a11',
         'Products.CMFPlone > 5.0.8', #for tinymce 4.4.3 (mockup >= 2.4.0). and need branch=5.0.x-include-ajax-haed
     ],
