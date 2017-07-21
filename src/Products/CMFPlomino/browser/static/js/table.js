9695d573ea2a204be016398d43df085f66aef613
Ivan Teoh <ivan.teoh@gmail.com>
2017-07-21 17:23:11 +1000

    Add pattern option hideCheckboxes

--- src/Products/CMFPlomino/browser/static/js/table.js
+++ src/Products/CMFPlomino/browser/static/js/table.js
@@ -26,11 +26,13 @@ require([
                     for(var i=0; i<data.rows.length; i++) {
                         var row = data.rows[i];
                         html += '<tr>';
-                        html += '<td>' +
-                            '<input type="checkbox" name="sdoc" value="' +
-                            row[0] +
-                            '" />' +
-                            '</td>';
+                        html += '<td>';
+                        if (self.options.hideCheckboxes !== 'True') {
+                            html += '<input type="checkbox" name="sdoc" value="' +
+                                row[0] +
+                                '" />';
+                        }
+                        html += '<td>';
                         html += '<td><a href="' +
                             self.options.source +
                             '/../document/' + row[0] +
