0f31d0636745499849c18b6d39dba3e35814a8f9
quang.nguyen <quang@pretagov.com>
2017-08-24 21:19:33 +1000

    Add missing macros from old format

---
+++ src/Products/CMFPlomino/defaultmacros/macros/macro_condition_compare_userdata.formula.py
@@ -0,0 +1,26 @@
+## START formula {
+PLONE = """
+
+member_id = \"\"\"{member_id}\"\"\"
+field_name = \"\"\"{field_name}\"\"\"
+value_to_compare = \"\"\"{value_to_compare}\"\"\"
+
+from Products.CMFCore.utils import getToolByName 
+mt = getToolByName(plominoContext, 'portal_membership') 
+member = mt.getMemberById(member_id)
+
+value = member.getProperty(field_name)
+return value is not None and value_to_compare is not None and value == value_to_compare
+"""
+
+doc = plominoContext
+member_id = doc.getItem('userlist', '')
+field_name = doc.getItem('field-name')
+value_to_compare = doc.getItem('value-to-compare', '')
+
+# We only handle Plone as a data-source for now
+code = PLONE.format(field_name=field_name, member_id=member_id, value_to_compare=value_to_compare)
+
+return code
+## END formula }
+
---
+++ src/Products/CMFPlomino/defaultmacros/macros/macro_field_select_userdata.field-name.py
@@ -0,0 +1,7 @@
+## START selectionlistformula {
+from Products.CMFCore.utils import getToolByName
+tool = getToolByName(plominoContext, 'portal_memberdata')
+ids = tool.propertyIds()
+return [''] + ids
+## END selectionlistformula }
+
