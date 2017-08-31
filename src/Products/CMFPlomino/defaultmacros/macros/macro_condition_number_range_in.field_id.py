688129917b840f889a2a5d2923ae86a23ae5a111
quang.nguyen <quang@pretagov.com>
2017-08-31 17:13:14 +1000

    Add current field selection to macro selection feild elements

--- src/Products/CMFPlomino/defaultmacros/macros/macro_condition_number_range_in.field_id.py
+++ src/Products/CMFPlomino/defaultmacros/macros/macro_condition_number_range_in.field_id.py
@@ -1,8 +1,7 @@
-## START formula {
-doc = plominoContext
-type_ = doc.getItem('element_type')
-include_other_ = doc.getItem('include_other', False)
-code = """
+## START selectionlistformula {
+
+### START macro_field_selection_db_elements_3 ###
+
 defaultitems = ['Select...|']
 editpath = plominoContext.REQUEST.get('Plomino_Macro_Context')
 Log('editpath %s' % editpath, 'macro_field_selection_db_elements/selectionlistformula', severity='debug')
@@ -25,66 +24,7 @@ def item(elm, form_name=''):
     if form_name:
         return '{title} ({id}) from form: {form_name}|{id}'.format(id=elm.id, title=elm.Title(), form_name=form_name)
     return '{title} ({id})|{id}'.format(id=elm.id, title=elm.Title())
-"""
 
-if type_ == 'Forms':
-    return code + """
-items = defaultitems
-for f in editdb.getForms():
-    try:
-        if f.getPortalTypeName() == "PlominoForm":
-            items.append(item(f))
-    except AttributeError:
-        continue
-return items
-"""
-elif type_ == 'Views':
-    return code + """
-items = defaultitems
-for f in editdb.getViews():
-    try:
-        if f.getPortalTypeName() == "PlominoView":
-            items.append(item(f))
-    except AttributeError:
-        continue
-return items
-"""
-elif type_ == 'Hidewhens':
-    return code + """
-items = defaultitems
-for f in editform.getHidewhenFormulas():
-    try:
-        if f.getPortalTypeName() == "PlominoHidewhen":
-            items.append(item(f))
-    except AttributeError:
-        continue
-return items
-"""
-elif type_ == 'Actions':
-    return code + """
-items = ['Save (save)|plomino_save','Close (close)|plomino_close']
-if editform:
-    for f, method_name in editform.getActions(hide=False):
-        try:
-            if f.getPortalTypeName() == "PlominoAction":
-                items.append(item(f))
-        except AttributeError:
-            continue
-    return items
-elif ctype == 'PlominoView':
-    for f, method_name in editcontext.getActions(hide=False):
-        try:
-            if f.getPortalTypeName() == "PlominoAction":
-                items.append(item(f))
-        except AttributeError:
-            continue
-    return items
-else:
-    return []
-"""
-elif type_ == 'Fields':
-    if include_other_:
-        return code + """
 def get_fields(form_element, form_name):
     field_items = defaultitems
     for this_form in form_element.getFormFields():
@@ -119,23 +59,7 @@ for other_form in editdb.getForms():
     except AttributeError:
         continue
 return current_form_items + other_form_items
-"""
-    else:
-        return code + """
-items = defaultitems
-if ctype == 'PlominoField':
-    items.append('Current field |%s' % '@@CURRENT_FIELD')
-for f in editform.getFormFields():
-    try:
-        if f.getPortalTypeName() == "PlominoField":
-            items.append(item(f))
-    except AttributeError:
-        continue
 
-return items
-
-"""
-else:
-    return code + 'return defaultitems'
-## END formula }
+### END macro_field_selection_db_elements_3 ###
+## END selectionlistformula }
 
